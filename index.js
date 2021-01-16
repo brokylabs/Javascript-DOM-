8898457ea22152b5a4f47b741354eeca66ddb3bc
Catur Hidayat <c.meong@gmail.com>
2021-01-16 16:24:36 +0700

    JS dibrowser - internal dan external, Function

---
+++ index.js
@@ -0,0 +1,21 @@
+function panggil(){
+    // console.log("Javascript Internal")
+    console.log("Menulis kode Javascript External")
+}
+
+function fungsiKedua(){
+    var data1 = "Purbalingga"
+    var data2 = "Klaten"
+
+    gabung = data1 + " " + data2
+
+    console.log(gabung);
+}
+
+
+
+
+
+
+fungsiKedua()
+panggil()
