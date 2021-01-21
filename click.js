7dcf2d16d5ffef16d4f4340ed3d3b2bf699b0aae
Catur Hidayat <c.meong@gmail.com>
2021-01-21 20:36:11 +0700

    double click

--- click.js
+++ click.js
@@ -1,3 +1,8 @@
 function clickMe(){
     document.getElementById("clicked").innerHTML = "Sudah!"
 }
+
+function doubleClick(){
+    document.getElementById("dbclick").innerHTML = "Already Click!"
+    document.body.style.backgroundColor = "cyan"
+}
